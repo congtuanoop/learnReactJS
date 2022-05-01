e03a09053cba7ee1f18dd4bc4b2c7e5b1c381ce0
congtuanoop <congtuanoop@gmail.com>
2022-05-01 12:55:45 +0700

    Render Element 01 05

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const name = "Truong Cong Tuan";
+  const age = 18;
+  const isFemale = false;
+  const student = {
+    name: 'Easy Frontend'
+  }
+  const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          congtuanoop@gmail.com
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>Hello {name} {isFemale ? 'Female' : 's'}</p>
+        {isFemale ? <p>Male</p> : <p>Female</p>}
+
+        {isFemale && <p>$Male</p>}
+
+        {isFemale && (
+          <div>
+            <p> &&Male 1</p>
+            <p> &&Male 2</p>
+          </div>
+        )}
+
+        {isFemale && (
+          <React.Fragment>
+            <p>Fragment Male 1</p>
+            <p>Fragment Male 2</p>
+          </React.Fragment>
+        )}
+
+        {isFemale && (
+          <>
+            <p>Male 1</p>
+            <p>Male 2</p>
+          </>
+        )}
+
+        {!isFemale && <p>Female</p>}
+
+        <ul>
+          {colorList.map(color => (
+            <li style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
